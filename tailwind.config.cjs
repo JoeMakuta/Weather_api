95312beb6f876298524f7c0593484c92b585926c
Josue Makuta <makutajosue@gmail.com>
2022-10-04 12:15:42 +0200

    The background issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,9 @@ module.exports = {
 
       },
       backgroundImage: {
-        'MainBack': "url(./src/assets/pexels-eberhard-grossgasteiger-1287075.jpg)",
-        'SubMainBack': "url('./src/assets/pexels-sid-ali-2028885-1920x1920.jpg')",
-        'ThirdMainBack': "url('./src/assets/pexels-johannes-plenio-1118873-1600x1600.jpg')",
+        'MainBack': "url('./src/assets/background1.jpg')",
+        'SubMainBack': "url('./src/assets/background2.jpg')",
+        'ThirdMainBack': "url('./src/assets/background3.jpg')",
       }
     },
 
