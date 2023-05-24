ffb9592e4030f578a4f3d5b148147c1d22dd8a07
Josué Makuta <makutajosue@gmail.com>
2023-05-24 10:32:57 +0200

    Background issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'body': ['Ubuntu'],
-        'body1': ['Poppins']
-
+        body: ["Ubuntu"],
+        body1: ["Poppins"],
       },
       backgroundImage: {
-        'MainBack': "url('/src/assets/background1.jpg')",
-        'SubMainBack': "url('/src/assets/background2.jpg')",
-        'ThirdMainBack': "url('/src/assets/background3.jpg')",
-      }
+        MainBack: "url('./src/assets/background1.jpg')",
+        SubMainBack: "url('./src/assets/background2.jpg')",
+        ThirdMainBack: "url('./src/assets/background3.jpg')",
+      },
     },
-
   },
   plugins: [],
-}
+};
